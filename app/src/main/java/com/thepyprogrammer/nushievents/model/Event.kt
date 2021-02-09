7d19f02269ee77f4901624b9df58731669f9ca47
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-09 15:54:31 +0800

    Create Event.kt

---
+++ app/src/main/java/com/thepyprogrammer/nushievents/model/Event.kt
@@ -0,0 +1,10 @@
+package com.thepyprogrammer.nushievents.model
+
+data class Event(
+    val title: String,
+    val dates: MutableList<TimeRange>,
+    val description: String,
+    val info: String
+) {
+
+}
