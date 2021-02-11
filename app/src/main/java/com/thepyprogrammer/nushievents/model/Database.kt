2f6f9e06f54e945fb04196f172e1afa4fae5ac44
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-11 17:58:19 +0800

    Update Database.kt

--- app/src/main/java/com/thepyprogrammer/nushievents/model/Database.kt
+++ app/src/main/java/com/thepyprogrammer/nushievents/model/Database.kt
@@ -4,8 +4,6 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.InputStream
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 class Database internal constructor(jsonStream: InputStream) : ArrayList<Event>() {
     companion object {
@@ -17,11 +15,11 @@ class Database internal constructor(jsonStream: InputStream) : ArrayList<Event>(
     init {
         val sc = Scanner(jsonStream)
         val s = StringBuilder()
-        while(sc.hasNext()) s.append(sc.nextLine())
+        while (sc.hasNext()) s.append(sc.nextLine())
         sc.close()
         val json = s.toString()
         val gson = Gson()
-        val sType = object : TypeToken<List<GsonEvent>>() { }.type
+        val sType = object : TypeToken<List<GsonEvent>>() {}.type
         gson.fromJson<List<GsonEvent>>(json, sType)?.forEach {
             val ev = it.toEvent()
             add(ev)
