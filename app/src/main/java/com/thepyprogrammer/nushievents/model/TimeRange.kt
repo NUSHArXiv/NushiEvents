deecd764dfe8cbb72ff8be2c846cc685fd1e92c3
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-11 17:59:47 +0800

    Update TimeRange.kt

--- app/src/main/java/com/thepyprogrammer/nushievents/model/TimeRange.kt
+++ app/src/main/java/com/thepyprogrammer/nushievents/model/TimeRange.kt
@@ -2,24 +2,24 @@ package com.thepyprogrammer.nushievents.model
 
 import java.time.LocalDate
 import java.time.LocalTime
-import java.util.regex.*
+import java.util.regex.Pattern
 
 class TimeRange(
-    val date: LocalDate,
-    val start: LocalTime,
-    val end: LocalTime
-    ) {
+        val date: LocalDate,
+        val start: LocalTime,
+        val end: LocalTime
+) {
 
     companion object {
         fun fromString(s: String): TimeRange? {
             val p =
-                Pattern.compile("((\\d{2})/(\\d{2})/(\\d{4})) ((\\d{2})(\\d{2})) - ((\\d{2})(\\d{2}))")
+                    Pattern.compile("((\\d{2})/(\\d{2})/(\\d{4})) ((\\d{2})(\\d{2})) - ((\\d{2})(\\d{2}))")
             val m = p.matcher(s)
             if (!m.matches()) return null
             val date = LocalDate.of(m.group(4).toInt(), m.group(3).toInt(), m.group(2).toInt())
             val start = LocalTime.of(m.group(6).toInt(), m.group(7).toInt())
             val end = LocalTime.of(m.group(9).toInt(), m.group(10).toInt())
-            return TimeRange(date, start, end);
+            return TimeRange(date, start, end)
         }
     }
 
