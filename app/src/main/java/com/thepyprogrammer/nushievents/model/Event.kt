7d05199c403ad084440462adaa038f57ea7a5922
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-14 10:48:10 +0800

    Update Event.kt

--- app/src/main/java/com/thepyprogrammer/nushievents/model/Event.kt
+++ app/src/main/java/com/thepyprogrammer/nushievents/model/Event.kt
@@ -1,8 +1,8 @@
 package com.thepyprogrammer.nushievents.model
 
 data class Event(
-        val title: String,
-        val dates: MutableList<TimeRange>,
-        val description: String,
-        val info: String
+    val title: String,
+    val dates: MutableList<TimeRange>,
+    val description: String,
+    val info: String
 )
