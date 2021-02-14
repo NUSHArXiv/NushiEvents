8a1f49ed87f3a9c50407cc55e703893683506224
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-14 10:48:15 +0800

    Update GsonEvent.kt

--- app/src/main/java/com/thepyprogrammer/nushievents/model/GsonEvent.kt
+++ app/src/main/java/com/thepyprogrammer/nushievents/model/GsonEvent.kt
@@ -3,15 +3,15 @@ package com.thepyprogrammer.nushievents.model
 import com.google.gson.annotations.SerializedName
 
 data class GsonEvent(
-        @SerializedName("title") val title: String,
-        @SerializedName("date") val dates: MutableList<String>,
-        @SerializedName("info") val info: String
+    @SerializedName("title") val title: String,
+    @SerializedName("date") val dates: MutableList<String>,
+    @SerializedName("info") val info: String
 ) {
     fun toEvent(): Event {
         val event = Event(title, mutableListOf(), "", info)
         val desc = StringBuilder()
         dates.forEach { date ->
-            TimeRange.fromString(date)?.let{
+            TimeRange.fromString(date)?.let {
                 event.dates.add(it)
                 desc.append(it + "\n")
             }
