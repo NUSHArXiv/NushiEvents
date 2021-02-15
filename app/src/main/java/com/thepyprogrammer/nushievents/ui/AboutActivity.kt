a17d0fb7b7d64026531a7df2733415b135f58ac1
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-15 22:18:19 +0800

    Update AboutActivity.kt

--- app/src/main/java/com/thepyprogrammer/nushievents/ui/AboutActivity.kt
+++ app/src/main/java/com/thepyprogrammer/nushievents/ui/AboutActivity.kt
@@ -1,6 +1,8 @@
 package com.thepyprogrammer.nushievents.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.thepyprogrammer.nushievents.R
 
@@ -8,5 +10,23 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        // Show the Up button in the action bar.
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            android.R.id.home -> {
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+
+                navigateUpTo(Intent(this, ItemListActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
