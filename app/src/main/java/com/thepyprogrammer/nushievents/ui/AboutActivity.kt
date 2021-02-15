c9d4d32d151294ed42546c475c04e00ce0296f9b
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-15 22:05:24 +0800

    Update AboutActivity.kt

--- app/src/main/java/com/thepyprogrammer/nushievents/ui/AboutActivity.kt
+++ app/src/main/java/com/thepyprogrammer/nushievents/ui/AboutActivity.kt
@@ -1,7 +1,7 @@
 package com.thepyprogrammer.nushievents.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.thepyprogrammer.nushievents.R
 
 class AboutActivity : AppCompatActivity() {
